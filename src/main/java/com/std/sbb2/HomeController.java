76338356cc000c60cb840b656a3a8fde6665f112
dudezi <a01036952858@gmail.com>
2024-09-03 11:47:23 +0900

    feat:수정기ë다시시작 ãmain 추가

--- src/main/java/com/std/sbb2/HomeController.java
+++ src/main/java/com/std/sbb2/HomeController.java
@@ -2,63 +2,27 @@ package com.std.sbb2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    private int number;
-
-    HomeController () {
-        number = -1;
-    }
+    HomeController () {}
 
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain () {
         return "안녕하세요.";
     }
-
     @GetMapping("/home/main2")
     @ResponseBody
     public String showMain2 () {
         return "반갑습니다.";
     }
-
     @GetMapping("/home/main3")
     @ResponseBody
     public String showMain3 () {
         return "즐거웠습니다.";
     }
 
-    @GetMapping("/home/increase")
-    @ResponseBody
-    public int showIncrease () {
-        number++;
-        return number;
-    }
 
-    @GetMapping("/home/plus")
-    @ResponseBody
-    public int plus(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b){
-        return a + b;
-    }
-
-    @GetMapping("/home/minus")
-    @ResponseBody
-    public int minus(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b){
-        return a - b;
-    }
-
-    @GetMapping("/home/divide")
-    @ResponseBody
-    public int divide(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b){
-        return a / b;
-    }
-
-    @GetMapping("/home/multiply")
-    @ResponseBody
-    public int multiply(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b){
-        return a * b;
-    }
 }
