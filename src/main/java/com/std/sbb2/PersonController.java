5ecbc48a2106f68400a41e2771f2e6e5757b9367
dudezi <a01036952858@gmail.com>
2024-09-03 12:12:05 +0900

    feat:t:삭제기능수정기능추가

---
+++ src/main/java/com/std/sbb2/PersonController.java
@@ -0,0 +1,69 @@
+package com.std.sbb2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PersonController {
+    private int lastId;
+    List<Person> people;
+
+    PersonController () {
+        lastId = 0;
+        people = new ArrayList<>();
+    }
+
+    @GetMapping("/person/add")
+    @ResponseBody
+    public String addPerson (@RequestParam("name") String name, @RequestParam("age") int age) {
+        lastId++;
+        Person p = new Person(lastId, name, age);
+        people.add(p);
+        return String.format("%d번 사람이 추가되었습니다.", p.getId());
+    }
+
+    @GetMapping("/person/people")
+    @ResponseBody
+    public List<Person> showPeople () {
+        return people;
+    }
+
+    @GetMapping("/person/remove")
+    @ResponseBody
+    public String removePerson (@RequestParam("id") int id) {
+        Person p = null;
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).getId() == id) {
+                p = people.get(i);
+            }
+        }
+        if (p == null) {
+            return id + "번 사람이 존재하지 않습니다.";
+        }
+        people.remove(p);
+        return id + "번 사람이 삭제되었습니다.";
+    }
+
+    @GetMapping("/person/modify")
+    @ResponseBody
+    public String  modifyPerson (@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("age") int age) {
+        Person p = null;
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).getId() == id) {
+                p = people.get(i);
+            }
+        }
+
+        if (p == null) {
+            return id + "번 사람이 존재하지 않습니다.";
+        }
+        people.get(id-1).setName(name);
+        people.get(id-1).setAge(age);
+        return id + "번 사람이 수정되었습니다.";
+    }
+}
